a646051c881abc12a024cb955cf4469815c122e7
DESKTOP-AI6JAQU@za135 <za13579ch@gmail.com>
2021-05-19 17:41:25 +0800

    modified2

--- html/bookkeeping.php
+++ html/bookkeeping.php
@@ -7,10 +7,10 @@
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.css' rel='stylesheet' />
     <link href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.1/css/all.css' rel='stylesheet'>
     <link href="../css/bookkeeping.css" rel="stylesheet"/>
-    <script src="./test.js"></script>
+    <script src="../js/calender.js"></script>
   </head>
   <body>
     <div id='calendar'></div>
-    
+
   </body>
 </html>
--- js/calender.js
+++ js/calender.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-    themeSystem: 'bootstrap'
+    themeSystem: 'bootstrap',
+    
+    dateClick: function(info) {
+      
+
+
+
+      }
+    
+
+
+
+
+
+
     });
     calendar.render();
   });
