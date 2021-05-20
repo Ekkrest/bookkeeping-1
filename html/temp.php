dbbb4176cc467a2d09185dba03636e5e80483b1c
Ekkrest <abc96007@gmail.com>
2021-05-20 17:08:56 +0800

    update-2

---
+++ html/temp.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="../css/Css.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="../js/JavaScript.js"></script>
+        <meta charset="utf-8">
+        <title>temp</title>
+        <script>
+            document.addEventListener('DOMContentLoaded',changeToLogin())
+        </script>
+      </head>
+
+    <body >
+      <?php		
+	    $db_link= @mysqli_connect("localhost", "root", "rootroot", "final_project");
+
+	    $email = $_POST["email"];
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+			
+	    $sql = "INSERT INTO userlogin (UserName, UserPassword, UserEmail, UserApproved) VALUES ('$username','$password','$email','N')";
+	    mysqli_query($db_link, $sql);
+			
+	    mysqli_close($db_link);
+        ?>
+        註冊成功!!!
+
+    </body>
+</html>
--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,11 +1,18 @@
 function changeToSignUp(){
     window.location.href = "../html/signUpPage.php"; 
 }
-
 function changeToBookkeeping(){
     window.location.href = "../html/bookkeeping.php"; 
 }
-
 function changeToLogin(){
-    window.location.href = "../html/loginPage.php";    
+    setTimeout(()=> window.location.href = "../html/loginPage.php", 2000);
+}
+function changeToTemp(){
+    window.location.href = "../html/temp.php"; 
+}
+function changeToBookkeeping1(){
+    setTimeout(()=> window.location.href = "../html/bookkeeping.php", 2000);
+}
+function changeToTemp1(){
+    window.location.href = "../html/loginToBookkeeping.php";
 }
